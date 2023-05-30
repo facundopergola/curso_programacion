871518421e92173f8acb18a7abd6bb2b45aa61af
unknown <facupergola216@gmail.com>
2023-05-30 19:59:39 -0300

    added modelo

--- facu/objetos/carritodecomida/CarritoDeComida.py
+++ facu/objetos/carritodecomida/CarritoDeComida.py
@@ -1,6 +1,10 @@
 class CarritoDeComida():
-	def __init(self, name):
+	def __init(self, name, modelo):
 		self.name = name
+		self.modelo = modelo
 	
 	def get_name(self):
 		return self.name
+	
+	def get_modelo(self):
+		return self.modelo
