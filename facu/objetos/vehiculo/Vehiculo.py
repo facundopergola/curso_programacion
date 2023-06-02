2f8a2fa87309090cae2af29232052c7b8e67ec44
unknown <facupergola216@gmail.com>
2023-06-02 01:50:14 -0300

    ejercicios-nuevos

--- facu/objetos/vehiculo/Vehiculo.py
+++ facu/objetos/vehiculo/Vehiculo.py
@@ -1,7 +1,10 @@
 class Vehiculo():
-	def __init__(self, name, modelo):
+	def __init__(self, name, modelo, asientos, luces):
 		self.name = name
 		self.modelo = modelo
+
+		self.asientos = asientos
+		self.luces = luces
 
 	def get_modelo(self):
 		return self.modelo
@@ -15,7 +18,18 @@ class Vehiculo():
 	def set_name(self, name):
 		self.name = name
 
+	#NUEVO
+	def get_asientos(self):
+		return self.asientos
+	
+	def set_asientos(self, asientos):
+		self.asientos = asientos
 
+	def get_luces(self):
+		return self.luces
+	
+	def set_luces(self, luces):
+		self.asientos = luces
 
 class Avion(Vehiculo):
 	
@@ -25,7 +39,12 @@ class Avion(Vehiculo):
 
 	def despegar(self):
 		print("despegando..")
-	
+
+#NUEVO	
+class Avioneta(Avion):
+	pass
+
+
 class Barco(Vehiculo):
 	def navegar(self):
 		print("navegando..")
@@ -38,7 +57,7 @@ avion1 = Avion(modelo="747", name="el aguila blanca", cantidadDeAlas=2)
 print(avion1.get_modelo())
 avion1.despegar()
 
-"""
+
 barco1 = Barco(modelo="Crucero", name="Titanic")
 print(barco1.get_modelo())
 barco1.navegar()
@@ -47,4 +66,8 @@ bote1 = Bote(name="bote chico", modelo="Titanico")
 print(bote1.get_modelo())
 bote1.navegar()
 
-"""
+#NUEVO
+class Auto(Vehiculo):
+	def derrapar(self):
+		print("derrapando")
+
